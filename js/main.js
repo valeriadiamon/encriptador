68143f7ad9d38ea42c3a364ad6c3fd9e632c2a75
ValeriaDiaz <valeriadiamon@gmail.com>
2023-01-22 16:25:17 -0600

    functions added and css finished for desktop view

--- js/main.js
+++ js/main.js
@@ -1,14 +1,22 @@
-//document.getElementById("encripted-message").style.display = "none";
-//let textareaEnc = document.getElementById("encripted-message");
-let err1 = document.getElementById('err1')
+let err1 = document.getElementById('error')
+let error2 = document.getElementById("error2")
 
 let arrLetras = ['a','e','i', 'o', 'u']
 let arrReglas = ['ai','enter','imes', 'ober', 'ufat' ]
 
 function encript(){
+    error2.innerHTML = "<p></p>"
     let origMsj = document.getElementById('orginal-message').value;
+    //console.log("mensjae"+origMsj+"nada")
     let splitedMsj = origMsj.split('');
     let encriptedMsj = [];
+    if(origMsj==""){
+        document.getElementById("encripted-message").value = "";
+        document.getElementById("sin-mensaje").style.visibility = "visible"
+        document.getElementById("error").style.visibility = "visible"
+        document.getElementById("error").innerText = "Coloca aquí el mensaje a encriptar ⬇️"
+    }
+    else{
     for(i=0; i < splitedMsj.length; i++ ){
         let letra = splitedMsj[i];
         if(arrLetras.find(element => element == letra)){
@@ -19,102 +27,108 @@ function encript(){
             encriptedMsj.push(letra)
         }
     }
-    /*if(letra == "a"){
-            encriptedMsj.push('ai')
-        }
-        else if(letra == "e"){
-            encriptedMsj.push('enter')
-        }
-        else if(letra == "i"){
-            encriptedMsj.push('imes')
-        }
-        else if(letra == "o"){
-            encriptedMsj.push('ober')
-        }
-        else if(letra == "u"){
-            encriptedMsj.push('ufat')
-        }
-        else{
-            encriptedMsj.push(letra)
-        }*/
-        //console.log(encriptedMsj.join(''))
-    //textareaEnc.style.display = "inline";
     finalMsj = encriptedMsj.join('')
+    document.getElementById("sin-mensaje").style.visibility = "hidden"
     document.getElementById("encripted-message").value = finalMsj;
+    document.getElementById('orginal-message').value = "";
+    }
 }
 
+
 function desencript(){
+    error2.innerHTML = "<p></p>"
     let suma = 0;
     let desen = document.getElementById('orginal-message').value;
-    console.log("primer desencriptado"+desen)
+    document.getElementById('orginal-message').value = "";
+    //console.log("primer desencriptado"+desen)
     for(x=0; x < arrReglas.length; x++){
         if(desen.search(arrReglas[x])!= -1 ){
             //console.log("no fue -1")
             //console.log(desen)
             desen = desen.replaceAll(arrReglas[x], arrLetras[x])
-            console.log("al final"+desen)
+            //console.log("al final"+desen)
+            err1.style.visibility = "hidden";
+            document.getElementById("sin-mensaje").style.visibility = "hidden"
             document.getElementById("encripted-message").value = desen;
+            
         }
         else if(desen.search(arrReglas[x])== -1){
             suma = suma + 1;
             if(suma == 5){
                 //console.log("No se encontró mensaje para desencriptar")
-                document.getElementById("error2").innerHTML = "Coloca aquí el mensaje a desencriptar ⬇️"
+                //console.log(suma)
+                err1.style.visibility = "visible"
+                //document.getElementById("encripted-message").value = "";
+                err1.innerHTML = "<p>Coloca aquí el mensaje a desencriptar ⬇️</p>"
+                //document.getElementById("sin-mensaje").style.visibility = "visible"
             }
         }
-        
-        /*else{
-            document.getElementById("error2").innerHTML = "No se encontró mensaje para desencriptar"
-        }*/
     }
     //console.log(desen)
-    //console.log(textareaEnc)
-    //document.getElementById('encripted-message').innerHTML = desen;
-    
 }
 
+
 function check(e){
+        error2.innerHTML = "<p></p>"
         tecla = (document.all) ? e.keyCode : e.which;  //guarda el código ascii de la tecla
-    
         //Para permitir tecla de borrar backspace
         if (tecla == 8) {
             return true;
         }
-        // Patrón de entrada, en este caso solo acepta numeros y letras
+        // Patrón de entrada, en este caso letras minusculas, espacio y salto de linea
         patron = /[a-z\s\n]/;
         tecla_final = String.fromCharCode(tecla);//convierte el código de caracter ascci a caracter
         //console.log(tecla)
         
         imprime = patron.test(tecla_final);
+        if(imprime){
+            err1.style.visibility = "hidden";
+        }
+
         if(!imprime){
             if(tecla >= 65 && tecla <= 90){
                 //console.log("tecla dentro del if"+tecla)
-                err1.style.display = "inline-flex";
-                document.getElementById("error").innerHTML = 'Oops! No se admiten <b>mayúsculas</b><button class="close" onclick="cerrar()">X</button>';
+                err1.style.visibility = "visible";
+                err1.innerHTML = '<p>Oops! No se admiten <b> mayúsculas</b></p>';
             }
             else if(tecla >= 48 && tecla <= 57){
                 //console.log("tecla dentro del if"+tecla)
-                err1.style.display = "inline-flex";
-                document.getElementById("error").innerHTML = 'Oops! No se admiten <b>números</b><button class="close" onclick="cerrar()">X</button>';
+                err1.style.visibility = "visible";
+                err1.innerHTML = '<p>Oops! No se admiten <b> números</b></p>';
             }
             else{
-                err1.style.display = "inline-flex";
-                document.getElementById("error").innerHTML = 'Oops! No se admiten <b>acentos</b> ni <b>caracteres especiales</b><button class="close" onclick="cerrar()">X</button>';
+                err1.style.visibility = "visible";
+                err1.innerHTML = '<p>Oops! No se admiten <b> acentos </b> ni <b> caracteres especiales</b></p>';
                 //console.log("no se admiten caracteres especiales")
             }
         }
         return imprime;
 }
 
+
 function hide(){
     let hideCont = document.getElementById("orginal-message")
     hideCont.setAttribute('type','password')
 }
+
 function show(){
     let hideCont = document.getElementById("orginal-message")
     hideCont.setAttribute('type','text')
 }
-function cerrar(){
-    console.log("entro a close");
-    err1.style.display = "none";
+
+function copiar(){
+    if(document.getElementById("encripted-message").value == ""){
+        //console.log("sin mensaje para copiar")
+        error2.innerHTML = '<p>Sin mensaje para copiar</p>';
+    }
+    else{
+        var content = document.getElementById('encripted-message').value;
+    navigator.clipboard.writeText(content).then(() => {error2.innerHTML = '<p><b>Texto copiado!</b></p>' })
+    .catch(err => {error2.innerHTML = '<p>Funcion no soportada por el navegador😟</p>' })
+    }
+}
+
+function limpiar(){
+    document.getElementById("encripted-message").value = "";
+    document.getElementById("sin-mensaje").style.visibility = "visible"
 }
